731fb62499d530f69d22142926d0f6ea97045851
R-Madhan_1306 <madhan.r2021@nandhatech.org>
2025-03-05 23:17:26 +0530

    added

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Priyadharsini P </span>. A Computer Science student with hands-on experience in software development, IoT, and AI projects. I have beginner-level knowledge in Java, HTML, and CSS and love solving problems through innovative solutions. <br><br> I have completed internships at DreamWork Solutions and Six Phrase Edu Tech Pvt Ltd, gaining practical experience in Java programming and web development With strong communication, teamwork, and empathy, I strive to improve my technical expertise and build impactful solutions."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
